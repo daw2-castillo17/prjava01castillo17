f11ddf025a790847484a46dc739bd7b4e8688449
ubu16 <ubu16@ubuDVL-castillo17>
2017-01-25 12:43:58 +0100

    Versió 0.1 del projecte prjava01castillo17

---
+++ src/prjava01castillo17/Prjava01castillo17.java
@@ -0,0 +1,27 @@
+
+package prjava01castillo17;
+
+import java.io.*;
+
+public class Prjava01castillo17 {
+
+    public static void main(String[] args) throws IOException {
+        File f = new File("fitxer01castillo17.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>");bw.newLine();
+            bw.write("  <head>");bw.newLine();
+            bw.write("    <title>");bw.newLine();
+            bw.write("      Nova p&agrave;gina  web de castillo17");bw.newLine();
+            bw.write("    </title>");bw.newLine();
+            bw.write("  </head>");bw.newLine();
+            bw.write("  <body>");bw.newLine();
+            bw.write("    Nova p&agrave;gina web de castillo17");bw.newLine();
+            bw.write("  </body>");bw.newLine();
+            bw.write("</html>");bw.newLine();
+            bw.close();
+        }
+    }
+        
+}
+    
+
